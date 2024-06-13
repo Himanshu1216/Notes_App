b354b3e52f1b4000d9619c0548905f70c9d27eb0
Himanshu1216 <himanshu.student.eee21@itbhu.ac.in>
2024-06-13 16:35:27 +0530

    created MyNotes page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,25 @@
 
 import './App.css';
-import Header from './Header/Header';
-import Footer from './Footer/Footer';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import LandingPage from './screens/LandingPage/LandingPage';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import MyNotes from './components/MyNotes/MyNotes';
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
-        <div>
+        <BrowserRouter>
             <Header/>
             <main>
-                <LandingPage/>
+                <Routes>
+                    <Route path='/' element={<LandingPage />}/>
+                    <Route path='/MyNotes' element={<MyNotes />} />
+                </Routes>
             </main>
             <Footer/>
-        </div>
+        </BrowserRouter>
     )
 }
 
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,11 +1,11 @@
-import {Navbar, Nav, NavDropdown, Container, Form, Col, Row, Button} from 'react-bootstrap';
+import {Navbar, Nav, NavDropdown, Container, Form} from 'react-bootstrap';
 import './Header.css'
 
 function Header() {
   return (
     <Navbar bg='primary' expand="lg" className="bg-body-tertiary py-3" variant='dark'>
       <Container>
-        <Navbar.Brand href="/" style={{fontSize: '1.4rem'}}>Note Cloud</Navbar.Brand>
+        <Navbar.Brand href="/" style={{fontSize: '1.5rem'}}>Note Cloud</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className='m-auto'>
@@ -18,7 +18,7 @@ function Header() {
             </Form>
           </Nav>
           <Nav className="me-auto">
-            <Nav.Link href="#home" className='mx-2' style={{fontSize: '1.1rem'}}>My notes</Nav.Link>
+            <Nav.Link href="/MyNotes" className='mx-2' style={{fontSize: '1.1rem'}}>My notes</Nav.Link>
             <NavDropdown title="Himanshu" id="basic-nav-dropdown" style={{fontSize: '1.1rem'}}>
               <NavDropdown.Item href="#action/3.3" style={{fontSize: '1rem'}}>My profile</NavDropdown.Item>
               <NavDropdown.Divider />
